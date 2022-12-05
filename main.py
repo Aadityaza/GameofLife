de7d8fdfe87ecb910f704ad8de299e8e7cc45dfe
Aaditya Jha <45432533+Aadityaza@users.noreply.github.com>
2022-12-05 23:38:00 +0545

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,17 @@
     #3 alive
 
 # main
-from PIL import Image 
 import numpy as np
 import random
 
-vp = np.random.randint(255,size=(200,200,200))
-print(vp)
-
-im = Image.fromarray(vp, 'RGB')
-im.show()
 
 
-# vp = np.zeros((200,200),dtype='i,i,i')
-# m,n=vp.shape
-# for i in range(1,n):
-#     for j in range(m):
-#         r = random.randint(0,255)
-#         g = random.randint(0,255)
-#         b = random.randint(0,255)
-#         vp[i,j]=(r,g,b)
 
-# im = Image.fromarray(vp, 'RGB')
-# im.show()
+vp = np.random.randint(255, size=(200,200))
+print (vp)
+m,n=vp.shape
+
+# display the image
+from matplotlib import pyplot as plt
+plt.imshow(vp,interpolation='nearest')
+plt.show()
